3f9da8107be07c479491855b11bd885cd3e409ec
Madhav Verma <vermamadhav369@gmail.com>
2026-08-09 20:45:41 +0530

    Time: 7 ms (96.64%), Space: 19.6 MB (91.4%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, col, board, ans, leftrow, lowerdiagonal, upperdiagonal, n):
+        if col == n:
+            ans.append(board[:])
+            return
+
+        for row in range(n):
+            if (
+                leftrow[row] == 0
+                and lowerdiagonal[row + col] == 0
+                and upperdiagonal[n - 1 + col - row] == 0
+            ):
+                board[row] = board[row][:col] + "Q" + board[row][col + 1:]
+                leftrow[row] = 1
+                lowerdiagonal[row + col] = 1
+                upperdiagonal[n - 1 + col - row] = 1
+
+                self.solve(col + 1, board, ans, leftrow, lowerdiagonal, upperdiagonal, n)
+
+                board[row] = board[row][:col] + "." + board[row][col + 1:]
+                leftrow[row] = 0
+                lowerdiagonal[row + col] = 0
+                upperdiagonal[n - 1 + col - row] = 0
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = ["." * n for _ in range(n)]
+        leftrow = [0] * n
+        lowerdiagonal = [0] * (2 * n - 1)
+        upperdiagonal = [0] * (2 * n - 1)
+        self.solve(0, board, ans, leftrow, lowerdiagonal, upperdiagonal, n)
+        return ans
